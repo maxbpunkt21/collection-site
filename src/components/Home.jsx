70f72c688e2ea075cbee1c691dc3031397a43c43
Maximilian Büchsenschütz <maximilian-buechsenschuetz@gmx.de>
2023-02-27 22:10:49 +0100

    db Anpassung

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,8 @@ import Navbar from "./Navbar";
 
 
 
-const API_URL = "http://localhost:4000/cryptostrains";
+//const API_URL = "http://localhost:4000/cryptostrains";
+const API_URL = "https://papriko-db.herokuapp.com/";
 
 
 const Home = () => {
